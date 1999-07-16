1ed8ca50a4986877f278d67acdf024645a87dcb9
Sam Roberts <vieuxtech@gmail.com>
1999-07-16 16:00:58 +0000

    *** empty log message ***

--- all.pl
+++ all.pl
@@ -1,7 +1,7 @@
 
 $index = 1;
 
-while (0 == system("popcmd sroberts:Isimclts\@pop.uniserve.com retr $index > $index.txt"))
+while (0 == system("popcmd sroberts\@pop.uniserve.com retr $index > $index.txt"))
 {
 	$index++;
 }
