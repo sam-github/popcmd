10c2d1fc96fd94186315cf1553691f8f934843cb
Sam Roberts <vieuxtech@gmail.com>
1999-07-16 15:59:56 +0000

    added help message

--- irange.cc
+++ irange.cc
@@ -3,11 +3,22 @@
 
 int main(int argc, char* argv[])
 {
+	if(argv[1] == 0 || strcmp(argv[1], "-h") == 0)
+	{
+		cout <<
+			"Usage: irange <-h|range>"
+			"\n"
+			"driver for testing the IntRange class\n"
+			;
+
+		exit(0);
+	}
+
 	IntRange ir;
 
 	if(!ir.Set(argv[1]))
 	{
-		cout << "failed to Set()" << endl;
+		cerr << "failed to Set()" << endl;
 		return 1;
 	}
 
